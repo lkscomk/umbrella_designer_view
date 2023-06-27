3758c80a90fb898dd8baf8eaff212720aaf7f15a
marinareginato <reginato0909@gmail.com>
2023-06-27 16:51:56 -0400

    atualização da página de finalizar pedido

--- src/views/finalizar_pedido/index.vue
+++ src/views/finalizar_pedido/index.vue
@@ -3,20 +3,38 @@
     <v-container fluid class="d-flex justify-center">
       <v-row>
         <v-col cols="6">
-          <v-img :src="require('@/assets/image-area.svg')" class="my-3" contain height="200" />
+          <v-card elevation="1" height="400" color="#D9D9D9" class="p-0 m-0">
+            <v-img :src="require('@/assets/image-area.svg')" class="my-3" contain height="380" />
+          </v-card>
         </v-col>
         <v-col cols="6">
           <v-col cols="12">
-            <div class="grey--text mb-2">Pedido Nº 21354 finalizado com sucesso!</div>
+            <v-card-title class="text-h5 p-0 m-0 d-flex justify-center">Pedido Nº 21354 finalizado com
+              sucesso!</v-card-title>
           </v-col>
           <v-col cols="12">
-            <v-text-field label="Subtitúlo*" outlined requerid hide-details></v-text-field>
+            <v-card elevation="1" height="230" color="#D9D9D9" class="p-0 m-0 justify-end">
+              <div class="mb-2  d-flex justify-end">Subtotal R$ 1.500,00</div>
+              <div class="mb-2  d-flex justify-end">Total R$ 1.500,00</div>
+            </v-card>
           </v-col>
           <v-col cols="12">
-            <v-text-field label="Subtitúlo*" outlined requerid hide-details></v-text-field>
+            <div class="text-center d-flex align-center justify-space-around">
+              <v-btn color="primary" dark v-bind="attrs" v-on="on">
+                Boleto
+              </v-btn>
+              <v-btn color="primary" dark v-bind="attrs" v-on="on">
+                Pix
+              </v-btn>
+              <v-btn color="primary" dark v-bind="attrs" v-on="on">
+                Débito
+              </v-btn>
+              <v-btn color="primary" dark v-bind="attrs" v-on="on">
+                Crédito
+              </v-btn>
+            </div>
           </v-col>
           <v-col cols="12">
-            <v-text-field label="Subtitúlo*" outlined requerid hide-details></v-text-field>
           </v-col>
         </v-col>
         <v-col cols="10" class="d-flex justify-end">
