219231af57164c009eb2dd7fe3e65ab8b4b6b7f4
lukas-ig <lukas.rocha@irmaosgoncalves.com.br>
2023-11-25 11:33:33 -0400

    tela acessos

--- src/views/acessos/store/mutations.js
+++ src/views/acessos/store/mutations.js
@@ -10,10 +10,10 @@ export default {
     })
     state.registros = param
   },
-  setRegistrosRelacionamento (state, param) {
+  setRegistrosTipo (state, param) {
     param.forEach(element => {
       element.created_at = element.created_at ? day(element.created_at).format('DD/MM/YYYY HH:mm:ss') : null
     })
-    state.registrosRelacionamento = param
+    state.registrosTipo = param
   }
 }
